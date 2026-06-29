1e4e77c07c8fa172682ec4788bb3517cb2e391bd
# Hi 👋 I'm Digvi  Frontend Developer | JavaScript & React Learner  - 🚀 Building real-world UI projects - 🌱 Learning MERN Stack - 💻 Focused on frontend development - 📫 Email: digvi2133@gmail.com <digvi2133@gmail.com>
2026-06-29 18:17:26 +0530

    Delete src directory

--- src/pages/Summary.jsx
+++
@@ -1,7 +0,0 @@
-export default function Summary() {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-white bg-black">
-      <h1 className="text-4xl font-bold">Summary Page</h1>
-    </div>
-  );
-}
